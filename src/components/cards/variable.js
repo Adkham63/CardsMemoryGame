bb18abf24e7507ca0db169cf4d26befd95b1bc2f
Adkham63 <karimovadham63@gmail.com>
2024-02-20 21:40:28 +0500

    first commit

---
+++ src/components/cards/variable.js
@@ -0,0 +1,62 @@
+export const CardImages = [
+    {
+        id:1 ,
+        url:"https://img.freepik.com/free-vector/crazy-universe-abstract-illustration_52683-87571.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:2 ,
+        url:"https://img.freepik.com/free-vector/crazy-style-space-illustration_52683-95523.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:3 ,
+        url:"https://img.freepik.com/free-vector/hand-drawn-halloween-illustration_23-2149643578.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:4 ,
+        url:"https://img.freepik.com/free-vector/creatures-illustration-stickers-set_516247-11.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:5 ,
+        url:"https://img.freepik.com/free-vector/crazy-universe-abstract-illustration_52683-87574.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:6 ,
+        url:"https://img.freepik.com/free-vector/hand-drawn-cartoon-bunny-face-illustration_52683-119978.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:7 ,
+        url:"https://img.freepik.com/free-vector/spooky-halloween-illustration-of-creatures_516247-4.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:8 ,
+        url:"https://img.freepik.com/free-vector/creative-and-colorful-creature-illustration_516247-6.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:9 ,
+        url:"https://img.freepik.com/free-vector/rave-party-sticker-set-design_23-2150318864.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:10 ,
+        url:"https://img.freepik.com/free-vector/terrible-barong-mask-hand-drawn-color-vector-doodle-illustration_179234-796.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:11 ,
+        url:"https://img.freepik.com/free-vector/doodle-cute-monster-sticker-icons-hand-drawn-coloring-vector_179234-346.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:12 ,
+        url:"https://img.freepik.com/free-vector/spooky-halloween-illustration-of-creatures_516247-8.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:13 ,
+        url:"https://img.freepik.com/free-vector/spooky-halloween-illustration-of-creatures_516247-5.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:14 ,
+        url:"https://img.freepik.com/free-vector/hand-drawn-galaxy-background_52683-143181.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+      {
+        id:15,
+        url:"https://img.freepik.com/free-vector/colorful-and-creepy-creatures-illustration-background_516247-10.jpg?size=626&ext=jpg&ga=GA1.1.425200563.1708358668&semt=ais"
+      },
+]
